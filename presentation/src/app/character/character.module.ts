0538b39f24af2f0a08513d5a588c977a2da3fdf9
G0dd4 <yannis.aissaoui@hotmail.fr>
2023-05-26 08:17:06 +0200

    Design data structure for character card

--- presentation/src/app/character/character.module.ts
+++ presentation/src/app/character/character.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CharacterCardComponent,
+    CharacterListComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ presentation/src/app/character/models/character-card.model.ts
@@ -0,0 +1,7 @@
+export interface Character {
+    Id:          number;
+    Firstname:   string;
+    Lastname:    string;
+    Image:       string;
+    Description: string;
+}
